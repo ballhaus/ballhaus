f9fbe641477d7847c0515cfdf2d44ade90999723
Adrian Lang <mail@adrianlang.de>
2013-09-25 10:59:50 +0200

    Move Performance tag

--- public/js/db.js
+++ public/js/db.js
@@ -317,12 +317,12 @@ app.factory('db',
                      }
                      db.tags = function () { return [ { name: 'Theater' },
                                                       { name: 'Tanz' },
+                                                      { name: 'Performance' },
                                                       { name: 'Film' },
                                                       { name: 'Musik' },
                                                       { name: 'Literatur' },
                                                       { name: 'Ausstellung' },
                                                       { name: 'Denken' },
-                                                      { name: 'Performance' },
                                                       { name: 'akademie der autodidakten' },
                                                       { name: 'project in/out' },
                                                       { name: 'Festival Black Lux' }
