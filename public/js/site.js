50327734ab8aa56aff32d739159450b5170ad5ea
Adrian Lang <mail@adrianlang.de>
2013-09-30 11:42:33 +0200

    Support home page teasers for pieces and events (closes #103)

--- public/js/site.js
+++ public/js/site.js
@@ -48,10 +48,12 @@ var app = angular.module('siteApp', ['ui.bootstrap', 'ngResource', '$strap.direc
     };
 })
 .filter('noTeaser', function () {
-    return function (input) {
-        return input && input.replace('\u06DD', '');
-    };
+    return removeTeaserMarker;
 });
+
+function removeTeaserMarker(input) {
+    return input && input.replace('\u06DD', '');
+};
 
 var language = 'de';
 
@@ -477,7 +479,7 @@ app.service('search', function (db, $q) {
           var objs = db.findObjects(c).map(function (obj) {
               obj = Object.create(obj);
               fields.forEach(function (f) {
-                  obj[f] = translate(obj[f]);
+                  obj[f] = removeTeaserMarker(translate(obj[f]));
               });
               return obj;
           });
