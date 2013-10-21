1f4b3bfc93a24a703f381b3d3170e9372ff74327
Hans Huebner <hans.huebner@gmail.com>
2013-10-21 17:08:30 +0200

    caching der db immer verhindern

--- app.js
+++ app.js
@@ -90,6 +90,9 @@ dirtyDb.on('load', function () {
 });
 
 app.get('/db', function (req, res) {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+    res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+    res.setHeader("Expires", "0"); // Proxies.
     res.send(icebox.freeze(db));
 });
 
