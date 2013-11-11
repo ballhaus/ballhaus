c2af82bab730bab157eb5317e5ff54ad6d075346
Hans Huebner <hans.huebner@gmail.com>
2013-11-11 19:47:51 +0100

    mit leuten ohne bilder klarkommen

--- public/js/site.js
+++ public/js/site.js
@@ -400,9 +400,11 @@ function PersonPageController($scope, db, $routeParams, Page) {
     $scope.person = db.get(db.Person, $routeParams.personId);
     if ($scope.person) {
         $scope.person = Object.create($scope.person);
-        $scope.person.images = $scope.person.images.map(function (img) {
-            return intoRect({height: 300, width: 300}, img);
-        });
+        if ($scope.person.images) {
+            $scope.person.images = $scope.person.images.map(function (img) {
+                return intoRect({height: 300, width: 300}, img);
+            });
+        }
     }
     Page.setTitle($scope.person ? $scope.person.name : 'Person nicht gefunden');
     Page.setSidebarContent('');
