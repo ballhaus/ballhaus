543e160a64ec132ec83562fc61b4b896c1bb5a20
Hans Huebner <hans.huebner@goeuro.com>
2014-07-30 17:24:47 +0200

    perform various checks before starting to prevent wtf moments

--- run.sh
+++ run.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
+if which -s gm
+then
+    :
+else
+    echo "please install graphicsmagick!"
+    exit 1
+fi
+
+if [ ! -f public/js/config.js ]
+then
+    echo "missing link to public/js/config.js"
+    exit 1
+fi
+
+if [ ! -f config.json ]
+then
+    echo "missing config.json"
+    exit 1
+fi
+
 while true
 do
 	node app
